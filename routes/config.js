67f1efd1122374e0adf7bb1f375c38bc297157e2
Xamarin <brahmaiah.chennupati@cognizant.com>
2016-05-26 17:55:50 +0530

    initail commit

---
+++ routes/config.js
@@ -0,0 +1,4 @@
+var URIs = { "redirecturi_homepage" : "http://MyRetailApplication.54.208.194.189.xip.io/userpage",
+"redirecturi" : "http://MyRetailApplication.54.208.194.189.xip.io"
+};
+module.exports = URIs;
